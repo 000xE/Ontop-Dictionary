0cdd5945bc3646cb6d390351d41688dfb03424ef
outerme <41651226+outerme@users.noreply.github.com>
2019-04-16 22:55:04 +0100

    Almost finished

--- OntopDictionary/Form1.Designer.cs
+++ OntopDictionary/Form1.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.HideSelection = false;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(473, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(0, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(473, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(0, 20);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(473, 233);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 276);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Opacity = 0.6D;
             this.Text = "Form1";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- OntopDictionary/Form1.cs
+++ OntopDictionary/Form1.cs
@@ -7,14 +7,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Web;
 
 namespace OntopDictionary
 {
     public partial class Form1 : Form
     {
+        string url = "https://owlbot.info/api/v2/dictionary/";
+        string word;
+        string definition;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            findDefinition();
+        }
+
+        private void findDefinition()
+        {
+            if (textBox1.Text.Length > 0)
+            {
+                word = textBox1.Text;
+
+                WebClient wc = new WebClient();
+
+                definition = getFullDefinition(wc.DownloadString(url + word + "?format=json"));
+
+                textBox2.Text = definition;
+            }
+        }
+
+        private string getFullDefinition(string raw)
+        {
+            string definition = "";
+
+            if (raw.IndexOf("definition") != -1)
+            {
+                char[] charsToTrim = { '[', ']', '{', '}', ','};
+                string[] definitions = raw.Split('}');
+                foreach (string full in definitions)
+                {
+                    string trimmed = full.Trim(charsToTrim);
+                    definition += trimmed + "\r\n\r\n";
+                }
+                //definition = raw; //raw.Substring(raw.IndexOf("definition"));
+            }
+            else
+            {
+                definition = "No definition";
+            }
+
+            return definition;
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                findDefinition();
+            }
         }
     }
 }
